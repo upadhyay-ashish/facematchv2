8b597e451b920815cfe45de430809059f2bbf26c
Ashish Upadhyay <ashish.cse2010@gmail.com>
2011-07-23 18:07:40 +0530

    final

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,20 @@
 class User < ActiveRecord::Base
-   has_attached_file :avatar
-   validates :name, :presence => true
-   validates :email, :presence => true
-   validates :email, :uniqueness => true
-   validates_attachment_size :avatar, :less_than => 2.megabytes, :message => "should be less than 2MB"
-   validates_attachment_content_type :avatar, :content_type => ["image/gif","image/jpg","image/jpeg","image/png"], :message => 'invalid format'
-#   validates :avatar, :format => {:with => %r{\.(gif|jpg|jpeg|png)$}i, :message => 'invalid format' }
-   
-
-
-   def self.selected_users
+  has_attached_file :avatar
+  validates :name, :presence => true
+  validates :email, :presence => true
+  validates :email, :uniqueness => true
+  validates_attachment_size :avatar, :less_than => 2.megabytes, :message => "should be less than 2MB"
+  validates_attachment_content_type :avatar, :content_type => ["image/gif","image/jpg","image/jpeg","image/png"], :message => 'invalid format'
+  validates_format_of :email,:with => /^([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})$/i
+  def self.selected_users
     User.all.shuffle!.first(4)
-   end
+  end
 
-   def self.pic_user(users)
-     users.shuffle.first
-   end
+  def self.pic_user(users)
+    users.shuffle.first
+  end
 
-   def self.first_user(user)
+  def self.first_user(user)
     user.first
-   end
+  end
 end
